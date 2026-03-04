88f550c902b8d6e6d3ba169c5ed7c214243738e4
Robert Ligeza <robertligeza2@gmail.com>
2026-03-04 08:51:33 +0100

    Composer require mcamara

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,36 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
-Route::get('/', function () {
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [
+            'localeSessionRedirect',
+            'localizationRedirect',
+            'localeViewPath'
+        ]
+    ],
+    function () {
+        // Fortify auth routes
+        require base_path('vendor/laravel/fortify/routes/routes.php');
+
+        Route::get('/', function () {
+            return view('welcome');
+        })->name('home');
+
+        Route::view('dashboard', 'dashboard')
+            ->middleware(['auth', 'verified'])
+            ->name('dashboard');
+
+        require __DIR__.'/settings.php';
+
+    }
+);
+
+
+/* Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
@@ -10,4 +38,4 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-require __DIR__.'/settings.php';
+require __DIR__.'/settings.php'; */
